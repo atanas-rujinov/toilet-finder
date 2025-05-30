4bc45fa9eb380c5f63ee2e157f7dc87c4f9c51ed
atanas-roujinov <99557136+atanas-rujinov@users.noreply.github.com>
2025-05-30 22:50:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///toilets.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
-print("testing autojenkins3")
+print("testing autojenkins4")
 
 # Models
 class User(db.Model):
